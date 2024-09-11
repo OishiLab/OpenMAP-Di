96bbe587945143d4acd1c12cc1372d34f9fbe9e8
tomoshi <shuhei.t.c@gmail.com>
2024-09-11 12:45:19 -0400

    Update parcellation.py

--- parcellation.py
+++ parcellation.py
@@ -38,7 +38,7 @@ def main():
     #     )
     opt = create_parser()
     device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
-    cnet, ssnet, pnet_c, pnet_s, pnet_a, hnet_c, hnet_a = load_model(opt, device)
+    ssnet, pnet_c, pnet_s, pnet_a, hnet_c, hnet_a = load_model(opt, device)
 
     print("load complete !!")
     pathes = sorted(glob.glob(os.path.join(opt.i, "**/*.nii"), recursive=True))
